e9ec0e64ca96bee94d00efc1ffc1928be1482866
DHANUSH G <122260688+dhanush2k3@users.noreply.github.com>
2025-11-11 17:13:05 +0530

    Removed sensitive credentials in AccessTokenApi
    Replaced sensitive CLIENT_ID and CLIENT_SECRET with placeholders.

--- src/services/AccessTokenApi.ts
+++ src/services/AccessTokenApi.ts
@@ -1,8 +1,8 @@
 import { createApi ,fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 
-const CLIENT_ID = "6389c0b253bf40938d767d188caacaa3";
-const CLIENT_SECRET = "792d8196f9aa4a2394214b123de93ebc";
+const CLIENT_ID = "CLIENT_ID";
+const CLIENT_SECRET = "CLIENT_SECRET";
 
 export const AccessTokenApi =  createApi({
     reducerPath:"AccessTokenApi",
