277ed5fe88c5ca023dea88f9162254b30f3be1ac
DHANUSH G <122260688+dhanush2k3@users.noreply.github.com>
2025-11-11 17:14:27 +0530

    Update debounce function implementation
    Refactor debounce function to accept a fixed delay of 500ms.

--- src/services/debounce.ts
+++ src/services/debounce.ts
@@ -1,20 +1,3 @@
-
-
-// export function debounce<T extends (...args : any[]) => void>(func : T , delay : number) 
-// :(...args : Parameters<T>) => void{
-
-//     let timeoutid : NodeJS.Timeout;
-
-//     return (...args : Parameters<T>) => {
-//         clearTimeout(timeoutid);
-
-//         timeoutid = setTimeout(() => {
-//         func(...args);
-//         },delay)
-//     }
-// }
-
-
 export function debounce(
   func: (text: string) => void, 
 ): (text: string) => void {
