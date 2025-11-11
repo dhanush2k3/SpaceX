792294d3f459cafb46ce529240ed15e510ec072f
DHANUSH G <122260688+dhanush2k3@users.noreply.github.com>
2025-11-11 17:13:51 +0530

    Obscure Spotify API client credentials
    Replaced sensitive Spotify API credentials with placeholders.

--- src/services/SpotifyApi.ts
+++ src/services/SpotifyApi.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const CLIENT_ID = "6389c0b253bf40938d767d188caacaa3";
-const CLIENT_SECRET = "792d8196f9aa4a2394214b123de93ebc";
+const CLIENT_ID = "CLIENT_ID";
+const CLIENT_SECRET = "CLIENT_SECRET";
 
 export interface Melofy{
     token: string | null;
